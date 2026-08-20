ff9e9461debd436e5652b127d121f88a20432eb0
Ibrahim Shawabkeh <ibrahimshawabkeh073@gmail.com>
2026-08-20 23:34:54 +0300

    Submitting new code about dictionaries as electron properties example

---
+++ Dictionary_example.py
@@ -0,0 +1,10 @@
+electron = {
+"Name": "e-",
+"mass": 9.11e31,
+"charge": -1.602e-19,
+}
+mass = electron["mass"]
+charge = electron["charge"]
+name = electron["Name"]
+print (f"mass of an electron is {mass}, the charge is {charge}, and finally we write it's name as {name} to represent it especially in calculations")
+
